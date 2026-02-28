a3a919bd4db48ae33667c51c8f24c183bba919c6
Reddysekhar8333 <srsekhar8333@gmail.com>
2026-02-28 22:46:44 +0530

    index.html added

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -4,8 +4,10 @@ URL configuration for task_manager project.
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='frontend-home'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('tasks.urls')),
