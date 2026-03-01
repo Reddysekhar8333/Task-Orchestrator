74b757f15dc3ef8718922bd216591d8d68fb5b36
Reddysekhar8333 <srsekhar8333@gmail.com>
2026-03-01 17:48:06 +0530

    ...

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -3,10 +3,16 @@ URL configuration for task_manager project.
 """
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+def healthz(_request):
+    """Simple process-level health endpoint for container checks."""
+    return JsonResponse({'status': 'ok'})
+
 urlpatterns = [
+    path('healthz', healthz, name='healthz'),
     path('', TemplateView.as_view(template_name='index.html'), name='frontend-home'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
